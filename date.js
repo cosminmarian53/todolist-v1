64f729837733e5da46173a3476f54692e44f3943
Lavric Cosmin-Marian <cosmin.lavric@student.unitbv.ro>
2024-02-22 23:41:00 +0200

    First Commit

---
+++ date.js
@@ -0,0 +1,25 @@
+
+function getDate() {
+  const today = new Date();
+  const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+
+  let day = today.toLocaleDateString("en-US", options);
+
+  return day;
+}
+function getDay() {
+    const today = new Date();
+    const options = {
+      weekday: "long"
+    };
+  
+    let day = today.toLocaleDateString("en-US", options);
+  
+    return day;
+  }
+module.exports.getDate=getDate;
+module.exports.getDay=getDay;
